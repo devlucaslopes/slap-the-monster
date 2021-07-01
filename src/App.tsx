1d90292ae0fbabfeb21596a5f40212155b3b0e34
Lucas Lopes <dev.thuram@gmail.com>
2021-07-01 13:47:56 -0300

    config eslint + prettier + editorconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 function App() {
   return <h1>Slap the monster!</h1>
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
 
-import App from './App';
+import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
-reportWebVitals();
+reportWebVitals()
